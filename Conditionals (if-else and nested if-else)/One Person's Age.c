525c23c247a14fd694ae04179216b06227dfb713
Rubaba Binte Mahbub <rubababintemahbub2007@gmail.com>
2026-05-07 14:19:31 +0600

    Implement age classification logic
    Add age classification based on user input.

---
+++ Conditionals (if-else and nested if-else)/One Person's Age.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int main()
+{
+int age;
+  
+printf("Enter one persons age :");
+scanf("%d", &age);
+
+if(age<2){
+  printf("Infant");
+}
+  
+else if(age>=2 && age <10){
+  printf("Child");
+}
+  
+else if(age>=10 && age <20 {
+  printf("Teenage");
+}
+  
+else if (age>=20 && age<30){
+  printf("Adult");
+}
+  
+else {
+  printf("Old");
+}
+  
+return 0;
+}
