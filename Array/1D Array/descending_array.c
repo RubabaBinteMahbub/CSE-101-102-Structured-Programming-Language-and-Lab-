bb38eb88dbb6dec4b4aead94427823001f26dfe9
Rubaba Binte Mahbub <rubababintemahbub2007@gmail.com>
2026-05-07 21:16:37 +0600

    Add C program to sort array in descending order

---
+++ Array/1D Array/descending_array.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+int size,i,j,temp;
+  
+printf("Enter the array size : ");
+scanf("%d",&size);
+  
+int a[size];
+  
+printf("Enter the elements : ");
+for(i=0;i<size;i++){
+	scanf("%d",&a[i]);
+}
+
+for(i=0;i<size;i++){
+		for(j=i+1;j<size;j++){
+				if(a[i]<a[j]){
+				temp=a[i];
+				a[i]=a[j];
+				a[j]=temp;
+				}
+		}
+}
+
+printf("Given array in descending order :");
+for(i=0;i<size;i++){
+  printf(" %d",a[i]);
+}
+
+return 0;
+}
