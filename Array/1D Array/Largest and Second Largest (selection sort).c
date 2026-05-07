28073454338189f5159194b5716ca9ca45345756
Rubaba Binte Mahbub <rubababintemahbub2007@gmail.com>
2026-05-07 21:37:26 +0600

    Create Largest and Second Largest (selection sort).c

---
+++ Array/1D Array/Largest and Second Largest (selection sort).c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(){
+    int a[5],i,j,temp;
+
+    printf("Enter nums :");
+    for(i=0;i<5;i++){
+        scanf("%d",&a[i]);
+    }
+    
+   for(i=0;i<5;i++){
+        for(j=i+1;j<5;j++){
+            if(a[i]<a[j]){
+              temp=a[i];
+              a[i]=a[j];
+              a[j]=temp;
+            }
+        }
+    }
+    printf("Descending order :");
+    for(i=0;i<5;i++){
+        printf(" %d",a[i]);
+    }
+    printf("\nLargest = %d",a[0]);
+    printf("\nSecond largest = %d",a[1]);
+    
+    return 0;
+}
