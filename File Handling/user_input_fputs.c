51e7dbc17442dbff3adc125b129c95a580652755
Rubaba Binte Mahbub <rubababintemahbub2007@gmail.com>
2026-05-07 22:11:27 +0600

    Add user input handling to write to a file

---
+++ File Handling/user_input_fputs.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+  FILE *file;
+  char name[20];
+  
+  file=fopen("test.txt","a");
+  if(file==NULL){
+    printf("File doesn't exist.\n");
+  }
+  else{
+    printf("The file is opened.\n");
+    printf("Enter your full name : \n");
+    fgets(name,20,stdin);
+    name[strcspn(name,"\n")]='\0';
+    
+    fputs(name,file);
+    fputs("\n",file);
+    
+    fclose(file);
+  }
+  
+  return 0;
+}
